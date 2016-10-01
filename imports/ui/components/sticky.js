1aaafcb02013c97c03601ca81adf97c72043cf55
Paul Savignano <Paul.Savignano@gmail.com>
2016-10-01 09:23:46 -0700

    Add Sticky component to index page.

--- imports/ui/components/sticky.js
+++ imports/ui/components/sticky.js
@@ -4,14 +4,14 @@ export class Sticky extends Component {
   componentDidMount() {
     const setInitialHeights = (elements) => {
       elements.forEach.call(elements, (sticky) => {
-        sticky.setAttribute('data-sticky-inital', sticky.getBoundingClientRect().top)
+        sticky.setAttribute('data-sticky-initial', sticky.getBoundingClientRect().top)
       })
     }
     const stickies = document.querySelectorAll('[data-sticky]')
-    setinitialHeights(stickies)
+    setInitialHeights(stickies)
     document.addEventListener('scroll', () => {
       const top = document.documentElement.scrollTop || document.body.scrollTop
-      const bottom = document.documentElement.scrollHeight || document.body.scrollHeight;
+      const bottom = document.documentElement.scrollHeight || document.body.scrollHeight
       stickies.forEach.call(stickies, (sticky) => {
         const stickyInitial = parseInt(sticky.getAttribute('data-sticky-initial'), 10)
         const stickyEnter = parseInt(sticky.getAttribute('data-sticky-enter'), 10) || stickyInitial
